cbaa8b12ff408cf3aeefe64c1e73036f227520de
mk-working <mk25.working@gmail.com>
2025-11-15 13:07:13 +0700

    #4.provider: done

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
+import { NinjasService } from './ninjas.service';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+  // injection through constructor
+  constructor(private readonly ninjasService: NinjasService) {}
+
+  // GET /ninjas?type=fast
+  @Get()
+  getNinjas(@Query('weapon') weapon: 'stars' | 'nunchucks') {
+    return this.ninjasService.getNinjas(weapon);
+  }
+
+  @Get(':id')
+  getOneNinja(@Param('id') id: string) {
+    return this.ninjasService.getNinja(+id);
+    // +id = Number(id) - runtime type casting
+  }
+
+  @Post()
+  createNinja(@Body() createNinjaDto: CreateNinjaDto) {
+    return this.ninjasService.createNinja(createNinjaDto);
+  }
+
+  @Put(':id')
+  updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
+    return this.ninjasService.updateNinja(+id, updateNinjaDto);
+  }
+
+  @Delete(':id')
+  removeNinja(@Param('id') id: string) {
+    return this.ninjasService.removeNinja(Number(id));
+  }
+}
+
+// CRUD Operation System
